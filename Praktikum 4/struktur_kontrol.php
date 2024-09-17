5db7fef81769103a5697bfe06babcfc2ab33a8cd
NabeelNizam <abee237@gmail.com>
2024-09-17 20:39:48 +0700

    belajar struktur kontrol

---
+++ Praktikum 4/struktur_kontrol.php
@@ -0,0 +1,55 @@
+<?php
+$nilaiNumerik = 92;
+
+if ($nilaiNumerik >=90 && $nilaiNumerik <=100) {
+    echo"Nilai Huruf A";
+}if ($nilaiNumerik >=80 && $nilaiNumerik <=90) {
+    echo"Nilai Huruf B";
+}if ($nilaiNumerik >=70 && $nilaiNumerik <=80) {
+    echo"Nilai Huruf C";
+}if ($nilaiNumerik < 70) {
+    echo"Nilai Huruf D";
+}
+
+$jarakSaatIni = 0;
+$jarakTarget = 500;
+$peningkatanHarian = 30;
+$hari = 0;
+
+while ($jarakSaatIni < $jarakTarget) {
+    $jarakSaatIni += $peningkatanHarian;
+    $hari++;
+}
+
+echo "<br>Atlet tersebut memerlukan $hari hari untuk mencapai jarak 500 kilometer.";
+
+$jumlahLahan = 10;
+$tanamanPerLahan = 5;
+$buahPerTanaman = 10;
+$jumlahBuah = 0;
+
+for ($i = 1; $i <= $jumlahLahan; $i++) {
+    $jumlahBuah += ($tanamanPerLahan * $buahPerTanaman);
+}
+
+echo "<br>Jumlah buah yang akan dipanen adalah: $jumlahBuah";
+
+$skorUjian = [85, 92, 78, 96, 88];
+$totalSkor = 0;
+
+foreach ($skorUjian as $skor) {
+    $totalSkor += $skor;
+}
+
+echo "Total skor ujian adalah: $totalSkor";
+
+$nilaiSiswa = [85, 92, 58, 64, 90, 55, 88, 79, 70, 96];
+
+foreach ($nilaiSiswa as $nilai) {
+    if ($nilai < 60) {
+        echo "Nilai: $nilai (Tidak lulus) <br>";
+        continue;
+    }
+    echo "Nilai: $nilai (Lulus) <br>";
+}
+?>
