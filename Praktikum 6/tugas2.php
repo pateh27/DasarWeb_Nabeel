4c2b8e25a39496935a818f81ec28d12ca34afcfd
NabeelNizam <abee237@gmail.com>
2024-10-01 22:00:47 +0700

    Tugas tabel

--- Praktikum 6/tugas2.php
+++ Praktikum 6/tugas2.php
@@ -4,26 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Table Assignment</title>
-    <style>
-        table {
-        border-collapse: collapse;
-        border-spacing: 0;
-        width: 100%;
-        border: 1px solid #ddd;
-    }
-    th, td{
-        text-align: left;
-        padding: 16px;
-    }
-    tr:nth-child(even){
-        background-color: #f2f2f2;
-    }
-
-    </style>
+    <link rel="stylesheet" type="text/css" href="styleTable.css">
+    <script src="jquery-3.7.1.js"></script>
+    <script>
+        $(document).ready(function(){
+            $("#tables"). hide();
+            $("#klik").click(function(){
+                $("#tables"). slideToggle("slow");
+            });
+        });
+    </script>
 </head>
 <body>
     <h2>Data Siswa</h2>
-    <table>
+    <div id="klik">Click to show tables</div>
+    <table id="tables">
         <tr>
             <td>Nama</td>
             <td>Usia</td>
