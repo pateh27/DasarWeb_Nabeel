9b39bec0e167d482252214f4236e3df95cfb0a11
NabeelNizam <abee237@gmail.com>
2024-10-01 09:41:21 +0700

    Tugas List

---
+++ Praktikum 6/tugas.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Data Mahasiswa Array Multidimiensi</h2>
+    <?php
+    $mhs = array (
+        array("Dina", 123456, "Teknik Kimia", "dina@gmail.com"),
+        array("Dino", 9123124, "Teknik Listrik", "dino@gmail.com")
+    );
+    ?>
+    <ul>
+        <?php 
+        foreach ($mhs as $mahasiswa) {
+                echo "<li>" . $mahasiswa[0] . "</li>";
+                echo "<li>" . $mahasiswa[1] . "</li>";
+                echo "<li>" . $mahasiswa[2] . "</li>";
+                echo "<li>" . $mahasiswa[3] . "</li>";
+                echo "<br>";
+        }
+        ?>
+    </ul>
+</body>
+</html>
