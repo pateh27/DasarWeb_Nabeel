0d9e289153e93119816d0681c1980c060b81f883
NabeelNizam <abee237@gmail.com>
2024-10-01 08:07:57 +0700

    Memasukkan array ke tabel

---
+++ Praktikum 6/array_2.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TABEL</title>
+    <style>
+        table,th,td {
+            border: 1px solid black;
+        }
+        table{
+            background-color: white;
+        }
+        th{
+            width: 500px;
+            background-color: yellow;
+        }
+    </style>
+</head>
+<body>
+    <center>
+    <h2>DATA DOSEN POLINEMA</h2>
+    </center>
+    
+    <?php
+    $dosen = [
+        'nama' => 'Elok Nur Hamdana',
+        'domisili'=> 'Malang',
+        'jenis_kelamin'=> 'Perempuan',
+    ];
+    ?>
+    <table>
+        <tr>
+            <th>Nama</th>
+            <th>Domisili</th>
+            <th>Jenis Kelamin</th>
+        </tr>
+        <tr>
+            <td><?php echo"$dosen[nama]";  ?></td>
+            <td><?php echo"$dosen[domisili]";  ?></td>
+            <td><?php echo"$dosen[jenis_kelamin]";  ?></td>
+        </tr>
+    </table>
+</body>
+</html>
