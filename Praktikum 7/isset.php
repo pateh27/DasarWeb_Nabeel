3ec51fd13617638733d644b011ed0dc05f8a5fd9
NabeelNizam <abee237@gmail.com>
2024-10-08 08:37:01 +0700

    Belajar penggunaan isset

---
+++ Praktikum 7/isset.php
@@ -0,0 +1,15 @@
+<?php
+$umur;
+if (isset($umur) && $umur >= "18") {
+    echo"Anda sudah dewasa";
+} else {
+    echo "Anda belum dewasa atau variabel 'umur' tidak ditemukan";
+}  
+echo"<br>";
+$data = array("nama" => "Jane", "usia" => 25) ;
+if (isset($data['nama'])) {
+    echo'Nama: '.$data['nama'];
+} else {
+    echo "variabel 'nama' tidak ditemukan dalam array";
+}
+?>
