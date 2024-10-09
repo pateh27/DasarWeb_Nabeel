e22aa203a7a787da0f00961fe015a621d1173a42
NabeelNizam <abee237@gmail.com>
2024-10-09 11:02:49 +0700

    belajar fungsi empty

---
+++ Praktikum 7/empty.php
@@ -0,0 +1,14 @@
+<?php
+$myArray = array();
+if (empty($myArray)) {
+    echo"Array tidak terdefinisi atau kosong.";
+} else {
+    echo"Array terdefinisi dan tidak kosong";
+}
+echo"<br>";
+if (empty ($notEksistenVar)) {
+    echo "Variabel tidak terdefinisi atau kosong";
+} else {
+    echo "Variabel terdefinisi dan tidak kosong";
+}
+?>
