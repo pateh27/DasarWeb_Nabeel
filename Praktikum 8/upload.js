c7510a1f119e2bc56653987392dccdc3e5ea63f7
NabeelNizam <abee237@gmail.com>
2024-10-17 18:14:24 +0700

    Menghias form upload

--- Praktikum 8/upload.js
+++ Praktikum 8/upload.js
@@ -1,5 +1,13 @@
-$(document).ready(function() {
-    $('#upload-form').submit(function(e) {
+$(document).ready(function(){
+    $('#file').change(function(){
+        if (this.files.length > 0) {
+            $('#upload-button').prop('disabled', false).css('opacity', 1);
+        } else {
+            $('#upload-button').prop('disabled', true).css('opacity', 0.5);
+        }
+    });
+
+    $('#upload-form').submit(function(e){
         e.preventDefault();
 
         var formData = new FormData(this);
@@ -11,10 +19,10 @@ $(document).ready(function() {
             cache: false,
             contentType: false,
             processData: false,
-            success: function(response) {
+            success: function(response){
                 $('#status').html(response);
             },
-            error: function() {
+            error: function(){
                 $('#status').html('Terjadi kesalahan saat mengunggah file.');
             }
         });
